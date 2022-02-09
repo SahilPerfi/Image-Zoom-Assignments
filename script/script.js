157fbd32dd24893b1571508ff34aa22ed4b73081
SahilPerfi <sahil.perficient@gmail.com>
2022-02-09 12:41:18 +0530

    updated

--- script/script.js
+++ script/script.js
@@ -1,16 +1,15 @@
 $(document).ready(function () {
-    var zoom1 = 100;
-    $(".zoomIn").click(function () {
-      if (zoom1 < 500) {
-        zoom1 += 25;
-        $("#bagImageZoom").css("zoom",zoom1+"%");
-      }
-    });
-    $(".zoomOut").click(function () {
-      if (zoom1 > 100) {
-        zoom1 -= 25;
-        $("#bagImageZoom").css("zoom",zoom1+"%");
-      }
-    });
+  var zoom1 = 100;
+  $(".zoomIn").click(function () {
+    if (zoom1 < 500) {
+      zoom1 += 25;
+      $("#bagImageZoom").css("zoom", zoom1 + "%");
+    }
   });
-  
+  $(".zoomOut").click(function () {
+    if (zoom1 > 100) {
+      zoom1 -= 25;
+      $("#bagImageZoom").css("zoom", zoom1 + "%");
+    }
+  });
+});
